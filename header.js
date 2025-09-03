4dd1dc64c15913d7f1b764e5a0e46cb5df24d11a
Alex Harrison <alexander.harrison.r@gmail.com>
2025-09-02 18:51:23 -0600

    test

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.write(`
 <header>
-<spaced><a href="/index.html">melee.cool</a></spaced>
+<spaced><a href="/index.html">about</a></spaced>
 <spaced><a href="/rwing/index.html">rwing</a></spaced>
 </header>
 <hr>
