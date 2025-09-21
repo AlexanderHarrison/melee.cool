1f8a7be1102886ab215a136250401b083e1d90f0
Alex Harrison <alexander.harrison.r@gmail.com>
2025-09-21 16:38:22 -0600

    move over clips stuff, and add tls

---
+++ clips.c
@@ -0,0 +1,277 @@
+// TODO: comprehensive testing
+// TODO: write to disk
+// TODO: reallocation
+
+typedef U32 HashKey;
+typedef U32 EntryIdx;
+typedef EntryIdx *EntryTable;
+
+static struct timespec timer;
+static void timer_start(void) {
+    clock_gettime(CLOCK_MONOTONIC, &timer);
+}
+static void timer_elapsed(const char *label) {
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    t.tv_sec -= timer.tv_sec;
+    t.tv_nsec -= timer.tv_nsec;
+    
+    F64 time_ms = (F64)t.tv_sec * 1000.0 + (F64)t.tv_nsec / 1000000.0;
+    printf("%s: %.2fms\n", label, time_ms);
+}
+
+typedef struct TagEntry {
+    HashKey hash;
+    U32 table_size;
+    EntryTable table;
+} TagEntry;
+
+#define TABLESIZE (1024*1024)
+static TagEntry *tag_table;
+
+typedef struct Entry {
+    U32 meta_idx;
+    U32 tag_strings_idx;
+} Entry;
+
+#define METASIZE (10*1024*1024)
+static char *meta;
+static U32 meta_size;
+static Entry *entry_table;
+static U32 tag_strings_size;
+static char *tag_strings;
+static U32 meta_entry_count = 0; // skip 0 (null) entry
+
+#define TAGMAX 32
+typedef struct EntrySearch {
+    U32 tag_count;
+    TagEntry tag_entry[TAGMAX];
+    U32 tag_search_idx[TAGMAX];
+} EntrySearch;
+
+static HashKey murmur_32_scramble(U32 k) {
+    k *= 0xcc9e2d51;
+    k = (k << 15) | (k >> 17);
+    k *= 0x1b873593;
+    return k;
+}
+
+static HashKey hash_bytes(const U8* key, U64 len) {
+    // murmur 3
+    U32 h = 0x1b873593;
+    U32 k;
+    for (U64 i = len >> 2; i; i--) {
+        memcpy(&k, key, sizeof(U32));
+        key += sizeof(U32);
+        h ^= murmur_32_scramble(k);
+        h = (h << 13) | (h >> 19);
+        h = h * 5 + 0xe6546b64;
+    }
+    k = 0;
+    for (U64 i = len & 3; i; i--) {
+        k <<= 8;
+        k |= key[i - 1];
+    }
+    h ^= murmur_32_scramble(k);
+    h ^= (U32)len;
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    return h;
+}
+
+static TagEntry *lookup_tag(Str tag) {
+    // hash tag
+    HashKey key = hash_bytes((const U8*)tag.buf, tag.len);
+    
+    // lookup hash
+    HashKey idx = key & (TABLESIZE-1);
+    TagEntry *entry;
+    
+    while (1) {
+        entry = &tag_table[idx];
+        if (entry->hash == key) {
+            break;
+        } else if (entry->hash == 0) {
+            entry->hash = key;
+            entry->table = calloc(TABLESIZE, sizeof(*entry->table));
+            break;
+        }
+        idx = (idx + 1) & (TABLESIZE-1);
+    }
+    
+    return entry;
+}
+
+static void tag_entry(EntryIdx entry_idx, Str tag) {
+    TagEntry *tag_entry = lookup_tag(tag);
+    tag_entry->table[tag_entry->table_size++] = entry_idx;
+
+    memcpy(tag_strings + tag_strings_size, tag.buf, tag.len);
+    tag_strings_size += (U32)tag.len;
+    tag_strings[tag_strings_size++] = '\n';
+}
+
+static const char *entry_metadata(Entry *entry) {
+    return &meta[entry->meta_idx];
+}
+
+static const char *entry_tags(Entry *entry) {
+    return &tag_strings[entry->tag_strings_idx];
+}
+
+static EntryIdx create_entry(Str new_meta) {
+    EntryIdx entry_idx = meta_entry_count++;
+    
+    meta_size++; // leave previous metadata null-terminated
+    tag_strings_size++; // leave previous tag strings null-terminated
+    entry_table[entry_idx] = (Entry) { meta_size, tag_strings_size };
+    
+    // copy metadata immediately
+    U32 new_meta_len = (U32)new_meta.len;
+    memcpy(meta + meta_size, new_meta.buf, new_meta_len);
+    meta_size += new_meta_len;
+    
+    return entry_idx;
+}
+
+static void search_tag(EntrySearch *search, Str tag) {
+    // insert into tag list, sorted by increasing entry count
+    
+    TagEntry *tag_entry = lookup_tag(tag);
+    U32 table_size = tag_entry->table_size;
+    
+    U32 i = search->tag_count;
+    while (
+        i != 0
+        && search->tag_entry[i-1].table_size > table_size
+    ) {
+        search->tag_entry[i] = search->tag_entry[i-1];
+        i -= 1;
+    }
+    search->tag_entry[i] = *tag_entry;
+    search->tag_count++;
+}
+
+static Entry *search_next_tagless(EntrySearch *search) {
+    // Since there are no tags, this is just sorting by newest.
+    // We just use the first search_idx as an index into meta entries.
+    
+    EntryIdx search_i = search->tag_search_idx[0];
+    if (search_i >= meta_entry_count)
+        return NULL;
+
+    search->tag_search_idx[0]++;
+    EntryIdx i = meta_entry_count - search_i - 1;
+    return &entry_table[i];
+}
+
+static Entry *search_next(EntrySearch *search) {
+    if (search->tag_count == 0)
+        return search_next_tagless(search);
+
+    // TODO - when should we binary search instead of linear scan?
+
+    EntryIdx idx_min = 0xFFFFFFFF;
+    
+    U32 tag_i = 0;
+    U32 tag_count = search->tag_count;
+    while (1) {
+        TagEntry *tag_entry = &search->tag_entry[tag_i];
+        U32 tag_entry_count = tag_entry->table_size;
+        U32 tag_search_idx = search->tag_search_idx[tag_i];
+        if (tag_search_idx >= tag_entry_count)
+            return NULL;
+        U32 tag_entry_idx = tag_entry_count - tag_search_idx - 1;
+        
+        EntryIdx prev_tag_entry = tag_entry->table[tag_entry_idx];
+        
+        if (prev_tag_entry > idx_min) {
+            // try next entry for this tag
+            search->tag_search_idx[tag_i] += 1;
+        } else if (prev_tag_entry < idx_min) {
+            // idx_min entry not found for this tag, start again 
+            idx_min = prev_tag_entry;
+            tag_i = 0;
+        } else {
+            // idx_min entry found for this tag, check next tag 
+            tag_i += 1;
+            
+            // return if all tags found for this entry
+            if (tag_i == tag_count) {
+                search->tag_search_idx[0] += 1;
+                break;
+            }
+        }
+    }
+    return &entry_table[idx_min];
+}
+
+static void init_clip_tables(void) {
+    tag_table = calloc(TABLESIZE, sizeof(*tag_table));
+    entry_table = calloc(TABLESIZE, sizeof(*entry_table));
+    meta = calloc(METASIZE, sizeof(*meta));
+    tag_strings = calloc(METASIZE, sizeof(*tag_strings));
+}
+
+/*static void create_entries(U32 count, U32 tag_count) {
+    char metabuf[16] = "entry ";
+    char tagbuf[3] = { 0 };
+    U32 rng = 0x67236;
+    
+    for (U32 i = 0; i < count; ++i) {
+        U32 n = i;
+        for (U32 j = 0; j < 8; ++j) { 
+            metabuf[6 + j] = 'a' + (char)(n >> 28);
+            n <<= 4;
+        }
+
+        EntryIdx entry = create_entry(metabuf);
+        
+        for (U32 j = 0; j < tag_count; ++j) {
+            rng = hash_bytes((void*)&rng, sizeof(rng));
+            tagbuf[0] = 'a' + (char)((rng >> 4) & 0xf);
+            tagbuf[1] = 'a' + (char)((rng >> 8) & 0xf);
+            
+            if (tagbuf[0] == 'b' && (rng & 1))
+                tagbuf[0] = 'a';
+            tag_entry(entry, tagbuf);
+        }
+    }
+}
+
+int main(void) {
+    init_clip_tables();
+    
+    timer_start();
+    create_entries(1 << 18, 16);
+    timer_elapsed("create entries");
+
+    // EntryIdx entry1 = create_entry("test 1!");
+    // tag_entry(entry1, "tag1");
+    // tag_entry(entry1, "tag2");
+
+    // EntryIdx entry2 = create_entry("test 2!");
+    // tag_entry(entry2, "tag1");
+    // tag_entry(entry2, "tag3");
+    
+    EntrySearch search = { 0 };
+    search_tag(&search, "ab");
+    search_tag(&search, "ac");
+    search_tag(&search, "ba");
+    // search_tag(&search, "ad");
+    
+    timer_start();
+    while (1) {
+        const char *metadata = search_next(&search);
+        if (metadata == 0)
+            break;
+        printf("found '%s'\n", metadata);
+    }
+    timer_elapsed("search");
+    
+    return 0;
+}*/
+
--- server.c
+++ server.c
@@ -22,67 +22,129 @@ typedef size_t USize;
 typedef ssize_t ISize;
 typedef struct mg_str Str;
 
-// void mg_print_str(const char *preface, struct mg_str str) {
-//     printf("%s%.*s\n", preface, (int) str.len, str.buf);
-// }
+#include "clips.c"
+
+void mg_print_str(const char *preface, struct mg_str str) {
+    printf("%s%.*s\n", preface, (int) str.len, str.buf);
+}
+
+#define REPLYSIZE (10)
+static Str reply;
+static const char *reply_headers = "Content-Type: text/html; charset=utf-8\r\n";
+
+#define reply_push_const(s) do {\
+    const char _str[] = s;\
+    USize _len = sizeof(_str)-1;\
+    if (reply.len + _len <= REPLYSIZE)\
+        memcpy(reply.buf + reply.len, _str, _len);\
+    reply.len += _len;\
+} while (0)
+
+void reply_push(const char *str) {
+    USize str_len = strlen(str);
+    memcpy(reply.buf + reply.len, str, str_len);
+    reply.len += str_len;
+}
+
+void reply_push_slice(const char *buf, USize len) {
+    memcpy(reply.buf + reply.len, buf, len);
+    reply.len += len;
+}
+
+void reply_recent_clips(void) {
+    reply_push_const("<div id=clips hx-swap-oob=true>");
+    EntrySearch search = { 0 };
+    for (U32 clip_i = 0; clip_i != 10; ++clip_i) {
+        Entry *entry = search_next(&search);
+        if (entry == NULL)
+            break;
+        
+        reply_push_const("<div class=clip>");
+            reply_push_const("<div class=metadata>");
+                const char *metadata = entry_metadata(entry);
+                printf("- found %s\n", metadata);
+                reply_push(metadata);
+            reply_push_const("</div>");
+            reply_push_const("<div class=tags>");
+                const char *tags = entry_tags(entry);
+                while (*tags) {
+                    USize i = 0;
+                    for (; tags[i] != '\n'; ++i);
+                    reply_push_const("<div class=tag>");
+                        reply_push_slice(tags, i);
+                    reply_push_const("</div>");
+                    tags += i + 1;
+                }
+            reply_push_const("</div>");
+        reply_push_const("</div>");
+    }
+    reply_push_const("</div>");
+}
+
+void reply_send(struct mg_connection *c) {
+    if (reply.len <= REPLYSIZE) {
+        mg_http_reply(c, 200, reply_headers, "%.*s", (int)reply.len, reply.buf);
+    } else {
+        mg_http_reply(c, 400, reply_headers, "Response is too large for internal buffer.");
+    }
+    reply.len = 0;
+}
 
 void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
     if (ev == MG_EV_HTTP_MSG) {
         struct mg_http_message *hm = (struct mg_http_message *) ev_data;
         // printf("%.*s %.*s\n", (int)hm->method.len, hm->method.buf, (int)hm->uri.len, hm->uri.buf);
         
-        static struct mg_http_serve_opts opts = { .root_dir = "web_root" };
-        mg_http_serve_dir(c, hm, &opts);
-        
-        #if 0
-            if (mg_match(hm->method, mg_str("GET"), NULL)) {
-                if (mg_match(hm->uri, mg_str("/clips"), NULL)) {
-                    mg_http_serve_file(c, hm, "clips.html", &opts);
-                    printf("- Served clips.html\n");
-                }
-                else if (mg_match(hm->uri, mg_str("/htmx.js"), NULL)) {
-                    mg_http_serve_file(c, hm, "vendor/htmx.js", &opts);
-                    printf("- Served vendor/htmx.js\n");
-                }
-                else if (mg_match(hm->uri, mg_str("/recent-clips"), NULL)) {
-                    reply_recent_clips();
-                    reply_send(c);
-                }
-                else {
-                    mg_http_reply(c, 404, NULL, "");
-                    printf("- 404\n");
-                }
+        if (mg_match(hm->method, mg_str("GET"), NULL)) {
+            if (mg_match(hm->uri, mg_str("/htmx.js"), NULL)) {
+                static struct mg_http_serve_opts opts = { 0 };
+                mg_http_serve_file(c, hm, "vendor/htmx.js", &opts);
             }
-            else if (mg_match(hm->method, mg_str("POST"), NULL)) {
-                if (mg_match(hm->uri, mg_str("/post-clip"), NULL)) {
-                    Str metadata = mg_http_var(hm->body, mg_str("metadata"));
-                    EntryIdx entry = create_entry(metadata);
-                    mg_print_str("- Added entry: \n", metadata);
-                    
-                    Str tag1 = mg_http_var(hm->body, mg_str("tag1"));
-                    if (tag1.len)
-                        tag_entry(entry, tag1);
-                    Str tag2 = mg_http_var(hm->body, mg_str("tag2"));
-                    if (tag2.len)
-                        tag_entry(entry, tag2);
-                    Str tag3 = mg_http_var(hm->body, mg_str("tag3"));
-                    if (tag3.len)
-                        tag_entry(entry, tag3);
-                    
-                    reply_recent_clips();
-                    reply_send(c);
-                    printf("- Served recent clips\n");
-                }
+            else if (mg_match(hm->uri, mg_str("/recent-clips"), NULL)) {
+                reply_recent_clips();
+                reply_send(c);
             }
-        #endif
- 
-        return;
+            else {
+                static struct mg_http_serve_opts opts = { .root_dir = "web_root" };
+                mg_http_serve_dir(c, hm, &opts);
+            }
+        }
+        else if (mg_match(hm->method, mg_str("POST"), NULL)) {
+            if (mg_match(hm->uri, mg_str("/post-clip"), NULL)) {
+                Str metadata = mg_http_var(hm->body, mg_str("metadata"));
+                EntryIdx entry = create_entry(metadata);
+                // mg_print_str("- Added entry: \n", metadata);
+                
+                Str tag1 = mg_http_var(hm->body, mg_str("tag1"));
+                if (tag1.len)
+                    tag_entry(entry, tag1);
+                Str tag2 = mg_http_var(hm->body, mg_str("tag2"));
+                if (tag2.len)
+                    tag_entry(entry, tag2);
+                Str tag3 = mg_http_var(hm->body, mg_str("tag3"));
+                if (tag3.len)
+                    tag_entry(entry, tag3);
+                
+                reply_recent_clips();
+                reply_send(c);
+            }
+        }
+    } else if (ev == MG_EV_ACCEPT) {
+        struct mg_tls_opts opts = {
+            .cert = mg_unpacked("certs/server_cert.pem"),
+            .key = mg_unpacked("certs/server_key.pem")
+        };
+        mg_tls_init(c, &opts);
     }
 }
 
 int main(void) {
+    init_clip_tables();
+    reply.buf = calloc(REPLYSIZE, 1);
+
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
+    
     mg_http_listen(&mgr, "http://0.0.0.0:10000", ev_handler, NULL);
     while (true)
         mg_mgr_poll(&mgr, -1);
