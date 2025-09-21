ad9832df00341f6c59c4ed3539ce0505103cea7d
Alex Harrison <alexander.harrison.r@gmail.com>
2025-09-21 17:07:58 -0600

    change to https

--- server.c
+++ server.c
@@ -133,8 +133,8 @@ void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
         }
     } else if (ev == MG_EV_ACCEPT) {
         struct mg_tls_opts opts = {
-            .cert = mg_unpacked("./certs/domain.cert.pem"),
-            .key = mg_unpacked("./certs/private.key.pem")
+            .cert = mg_unpacked("certs/domain.cert.pem"),
+            .key = mg_unpacked("certs/private.key.pem")
         };
         mg_tls_init(c, &opts);
     }
@@ -147,7 +147,7 @@ int main(void) {
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
     
-    mg_http_listen(&mgr, "http://0.0.0.0:10000", ev_handler, NULL);
+    mg_http_listen(&mgr, "https://0.0.0.0:10000", ev_handler, NULL);
     while (true)
         mg_mgr_poll(&mgr, -1);
     mg_mgr_free(&mgr);
